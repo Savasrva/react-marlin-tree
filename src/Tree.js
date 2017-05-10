a16a24c0f3c98d18138223040f745e159cc1b3c5
Savasrva <mgk7098@gmail.com>
2017-05-10 12:05:56 +0900

    renaming functions
    ...

--- src/Tree.js
+++ src/Tree.js
@@ -8,63 +8,70 @@ export default class Tree extends React.Component {
 
   constructor(props) {
     super(props);
-    this._handleSubmit = this._handleSubmit.bind(this);
-    this._handleDeleteNode = this._handleDeleteNode.bind(this);
-    this._handleToggleLock = this._handleToggleLock.bind(this);
-    this._handleToggleEdit = this._handleToggleEdit.bind(this);
-    this._handleChange = this._handleChange.bind(this);
-    this._handleToggleFold = this._handleToggleFold.bind(this);
-    this._handleAddRootNode = this._handleAddRootNode.bind(this);
-    this._handleAddNode = this._handleAddNode.bind(this);
+    this._defaultData = this._defaultData.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDeleteNode = this.handleDeleteNode.bind(this);
+    this.handleToggleLock = this.handleToggleLock.bind(this);
+    this.handleToggleEdit = this.handleToggleEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleToggleFold = this.handleToggleFold.bind(this);
+    this.handleAddRootNode = this.handleAddRootNode.bind(this);
+    this.handleAddNode = this.handleAddNode.bind(this);
     this.state = {
-      nodeData: props.data
+      nodeData : props.data
     };
   }
 
-  _handleSubmit() {
+  _defaultData({target, selectedNode = false}) {
+    let defaultHandleData = {
+      nodeData : _.cloneDeep(this.state.nodeData),
+      index : closest(target, 'li').getAttribute('data-index').split('-')
+    };
+
+    if(selectedNode) {
+      defaultHandleData['selectedNode'] = selectData(defaultHandleData.nodeData, defaultHandleData.index);
+    }
+
+    return defaultHandleData;
+  }
+
+  handleSubmit() {
     this.props.onSubmit(JSON.stringify(this.state.nodeData));
   }
 
-  _handleDeleteNode({target}) {
-    const nodeData = _.cloneDeep(this.state.nodeData),
-      index = closest(target, 'li').getAttribute('data-index').split('-');
+  handleDeleteNode({target}) {
+    const {nodeData, index} = this._defaultData({target: target, selectedNode: false});
 
     deleteData(nodeData, index);
 
     this.setState({nodeData : nodeData});
   }
 
-  _handleToggleLock({target}) {
-    const nodeData = _.cloneDeep(this.state.nodeData),
-      index = closest(target, 'li').getAttribute('data-index').split('-'),
-      selectedNode = selectData(nodeData, index);
+  handleToggleLock({target}) {
+    const {nodeData, index, selectedNode} = this._defaultData({target: target, selectedNode: true});
 
     selectedNode.isLocked = !selectedNode.isLocked;
 
     this.setState({nodeData : nodeData});
   }
 
-  _handleToggleEdit({target}) {
-    const nodeData = _.cloneDeep(this.state.nodeData),
-      index = closest(target, 'li').getAttribute('data-index').split('-'),
-      selectedNode = selectData(nodeData, index);
+  handleToggleEdit({target}) {
+    const {nodeData, index, selectedNode} = this._defaultData({target: target, selectedNode: true});
 
     selectedNode.isEditMode = !selectedNode.isEditMode;
 
     this.setState({nodeData : nodeData});
   }
 
-  _handleChange({target}) {
-    const nodeData = _.cloneDeep(this.state.nodeData),
-      index = closest(target, 'li').getAttribute('data-index').split('-'),
-      selectedNode = selectData(nodeData, index);
+  handleChange({target}) {
+    const {nodeData, index, selectedNode} = this._defaultData({target: target, selectedNode: true});
 
     selectedNode.title = target.value;
 
     this.setState({nodeData : nodeData});
   }
 
-  _handleToggleFold({target}) {
+  handleToggleFold({target}) {
     const ul = target.parentElement.lastElementChild;
     target.className = target.className === 'button_list_unfold' ? 'button_list_fold' : 'button_list_unfold';
 
@@ -75,7 +82,7 @@ export default class Tree extends React.Component {
     }
   }
 
-  _handleAddRootNode() {
+  handleAddRootNode() {
     if(this.state.nodeData === undefined) {
       this.setState({nodeData: [initData()]});
       return;
@@ -86,10 +93,8 @@ export default class Tree extends React.Component {
     }));
   }
 
-  _handleAddNode({target}) {
-    const nodeData = _.cloneDeep(this.state.nodeData),
-      index = closest(target, 'li').getAttribute('data-index').split('-'),
-      selectedNode = selectData(nodeData, index);
+  handleAddNode({target}) {
+    const {nodeData, index, selectedNode} = this._defaultData({target: target, selectedNode: true});
 
     if(selectedNode.childNodes) {
       selectedNode.childNodes.push(initData());
@@ -100,7 +105,7 @@ export default class Tree extends React.Component {
     this.setState({nodeData : nodeData});
   }
 
-  _renderReadField(title) {
+  renderReadField(title) {
     return (
       <span className="read_area">
         {title}
@@ -108,92 +113,92 @@ export default class Tree extends React.Component {
     );
   }
 
-  _renderEditField(title) {
+  renderEditField(title) {
     return (
       <span className="input_area">
-        <input type="text" className="edit" onChange={this._handleChange} value={title}/>
+        <input type="text" className="edit" onChange={this.handleChange} value={title}/>
       </span>
     );
   }
 
-  _renderDefaultBtns() {
+  renderDefaultBtns() {
     return (
       <div className="button_group">
-        <button onClick={this._handleAddNode} type="button" className="button_add">ADD</button>
-        <button onClick={this._handleToggleLock} type="button" className="button_lock">LOCK</button>
-        <button onClick={this._handleToggleEdit} type="button" className="button_edit_off">EDIT</button>
-        <button onClick={this._handleDeleteNode} type="button" className="button_delete">DELETE</button>
+        <button onClick={this.handleAddNode} type="button" className="button_add">ADD</button>
+        <button onClick={this.handleToggleLock} type="button" className="button_lock">LOCK</button>
+        <button onClick={this.handleToggleEdit} type="button" className="button_edit_off">EDIT</button>
+        <button onClick={this.handleDeleteNode} type="button" className="button_delete">DELETE</button>
       </div>
     );
   }
 
-  _renderEditBtns() {
+  renderEditBtns() {
     return (
       <div className="button_group">
-        <button onClick={this._handleToggleEdit} type="button" className="button_edit_on">COMPLETE</button>
-        <button onClick={this._handleDeleteNode} type="button" className="button_delete">DELETE</button>
+        <button onClick={this.handleToggleEdit} type="button" className="button_edit_on">COMPLETE</button>
+        <button onClick={this.handleDeleteNode} type="button" className="button_delete">DELETE</button>
       </div>
     );
   }
 
-  _renderLockBtns() {
+  renderLockBtns() {
     return (
       <div className="button_group">
-        <button onClick={this._handleToggleLock} type="button" className="button_unlock">UNLOCK</button>
+        <button onClick={this.handleToggleLock} type="button" className="button_unlock">UNLOCK</button>
       </div>
     )
   }
 
-  _renderButtonGroup(isParentLocked, isLocked, isEditMode = false) {
+  renderButtonGroup(isParentLocked, isLocked, isEditMode = false) {
     if(isParentLocked) {
       return undefined;
     } else if(isLocked) {
-      return this._renderLockBtns();
+      return this.renderLockBtns();
     } else if(isEditMode) {
-      return this._renderEditBtns();
+      return this.renderEditBtns();
     } else {
-      return this._renderDefaultBtns();
+      return this.renderDefaultBtns();
     }
   }
 
-  _renderNode(node, keys, isParentLocked = false) {
+  renderNode(node, keys, isParentLocked = false) {
     const hasChild = _.isEmpty(node.childNodes) ? 'button_list_none' : 'button_list_fold',
       willChildLock = isParentLocked ? isParentLocked : node.isLocked;
     return (
       <li data-index={keys} key={keys}>
-        <button type="button" className={hasChild} onClick={this._handleToggleFold}>open</button>
+        <button type="button" className={hasChild} onClick={this.handleToggleFold}>open</button>
         <div className="contents_tree">
-          {this.props.isEditable ? this._renderButtonGroup(isParentLocked, node.isLocked, node.isEditMode) : undefined}
+          {this.props.isEditable ? this.renderButtonGroup(isParentLocked, node.isLocked, node.isEditMode) : undefined}
           <div className="text_area">
-            {node.isEditMode ? this._renderEditField(node.title) : this._renderReadField(node.title)}
+            {node.isEditMode ? this.renderEditField(node.title) : this.renderReadField(node.title)}
           </div>
         </div>
-        {!_.isEmpty(node.childNodes) ? this._renderNodeContainer(node.childNodes, keys, willChildLock) : undefined}
+        {!_.isEmpty(node.childNodes) ? this.renderNodeContainer(node.childNodes, keys, willChildLock) : undefined}
       </li>
     );
   }
 
-  _renderNodeContainer(nodes, depth , isParentLocked = false) {
+  renderNodeContainer(nodes, depth , isParentLocked = false) {
     return (
       <ul>
         {
           nodes.map((e, i) => {
             const keys = depth !== 'init' ? depth + '-' + i : i;
-            return this._renderNode(e, keys, isParentLocked);
+            return this.renderNode(e, keys, isParentLocked);
           })
         }
       </ul>
     );
   }
 
-  _renderAddRootArea() {
+  renderAddRootArea() {
     return (
       <div className="start_back">
         <ul>
           <li><p><strong>MARLIN TREE</strong></p></li>
           <li className="contents_start">
-            <button onClick={this._handleAddRootNode} type="button">MARLIN TREE START</button>
-            <button onClick={this._handleSubmit} type="submit">SUBMIT</button>
+            <button onClick={this.handleAddRootNode} type="button">MARLIN TREE START</button>
+            <button onClick={this.handleSubmit} type="submit">SUBMIT</button>
           </li>
         </ul>
       </div>
@@ -203,9 +208,9 @@ export default class Tree extends React.Component {
   render() {
     return (
       <div id="marlinTreeView">
-        {this.props.isEditable ? this._renderAddRootArea() : undefined}
+        {this.props.isEditable ? this.renderAddRootArea() : undefined}
         <div className="contents_wrap">
-          {this._renderNodeContainer(this.state.nodeData, 'init')}
+          {this.renderNodeContainer(this.state.nodeData, 'init')}
         </div>
       </div>
     );
